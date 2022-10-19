1925dd510719843a6c2216320249c3d3a6985604
Rafael Tavares <rafael-tavares@live.com>
2022-10-19 14:30:53 -0300

    adding baseResourceModel

--- src/app/pages/entries/shared/entry.model.ts
+++ src/app/pages/entries/shared/entry.model.ts
@@ -1,8 +1,9 @@
+import { BaseResourceModel } from "src/app/shared/models/base-resource.model";
 import { Category } from "../../categories/shared/category.model";
 
-export class Entry {
+export class Entry extends BaseResourceModel {
   constructor(
-    public id?: number,
+    public override id?: number,
     public name?: string,
     public description?: string,
     public type?: string,
@@ -11,7 +12,7 @@ export class Entry {
     public paid?: boolean,
     public categoryId?: number,
     public category?: Category
-  ){}
+  ){super()}
   static types = {
     expense: 'Despesa',
     revenue: 'Receita'
