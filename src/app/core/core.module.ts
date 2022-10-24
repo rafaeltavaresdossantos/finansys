2542b83f091edb414438e4106583b0c4f1398546
Rafael Tavares <rafael-tavares@live.com>
2022-10-24 14:58:41 -0300

    componenting navBar

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,21 +5,27 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { HttpClientModule } from '@angular/common/http';
 import { InMemoryDatabase } from '../in-memory-database';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { AppRoutingModule } from '../app-routing.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NavBarComponent
+  ],
   imports: [
     CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase),
+    AppRoutingModule
   ],
   exports:[
     BrowserModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    NavBarComponent
   ]
 })
 export class CoreModule { }
